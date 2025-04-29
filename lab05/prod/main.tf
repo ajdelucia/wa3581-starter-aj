9bdd7940e5a90ce1bee949dc51a623b5299bf465
ajdelucia <ajdelucia99@gmail.com>
2025-04-29 18:43:40 +0000

    feat(lab05): Configure dev and prod SQS environments

---
+++ lab05/prod/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+  # Backend configured by HCP Workspace
+}
+
+
+variable queue_prefix {
+    type = string
+}
+
+variable environment_tag {
+    type = string
+}
+
+module prod_queue {
+    source  = "app.terraform.io/tf-advanced-labs-ajdelucia/sqs-secure/aws"
+    version = "1.0.0"
+    
+    queue_name_prefix = var.queue_prefix
+    enable_dlq = true
+
+    tags = {
+        Project = "Advanced TF Course"
+        Environment = var.environment_tag
+    }
+}
